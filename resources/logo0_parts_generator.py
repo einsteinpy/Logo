33b5ea0589458ae9fec7da3074094948818083b0
Ritwik Saha <s.ritwik98@gmail.com>
2019-02-04 16:57:07 +0000

    Much Change Such WOW !!

--- resources/logo0_parts_generator.py
+++ resources/logo0_parts_generator.py
@@ -18,6 +18,110 @@ from einsteinpy import constant
 
 end_lambda=0.021
 
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+M = 5.972e24 * u.kg
+pos_vec = np.array([200.0, np.pi/2, 0.5*(np.pi/2+(np.pi+np.pi/6))])
+real_omega = 7600
+vel_vec = np.array([0, 0., real_omega])
+
+
+time = 0 * u.s
+cl = Schwarzschild.from_values(pos_vec, vel_vec, time, M)
+ANS = cl.calculate_trajectory(end_lambda=end_lambda/30, OdeMethodKwargs={'stepsize':0.2e-6})
+
+ans = ANS[1]
+print('calculate done')
+
+r = np.array([t[1] for t in ans])
+print(r[-1])
+
+phi = np.array([t[3] for t in ans])
+time = np.array([t[0] for t in ans])
+x = r * np.cos(phi)
+y = r*np.sin(phi)
+ax.scatter(x,y, s=0.4, c=time[::-1], cmap='RdPu')
+ax.scatter(0,0, color='black')
+
+plt.axis(xmin = -350, xmax=350, ymin=-350, ymax=350)
+ax.set_aspect(1)
+plt.savefig(fname='swoosh0.png', transparent=True, dpi=1500)
+
+
+
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+M = 5.972e24 * u.kg
+pos_vec = np.array([200.0, np.pi/2, 0.5*(np.pi/2+(np.pi+np.pi/6))+2*np.pi/3])
+real_omega = 7600
+vel_vec = np.array([0, 0., real_omega])
+
+
+time = 0 * u.s
+cl = Schwarzschild.from_values(pos_vec, vel_vec, time, M)
+ANS = cl.calculate_trajectory(end_lambda=end_lambda/30, OdeMethodKwargs={'stepsize':0.2e-6})
+
+ans = ANS[1]
+print('calculate done')
+
+r = np.array([t[1] for t in ans])
+print(r[-1])
+
+phi = np.array([t[3] for t in ans])
+time = np.array([t[0] for t in ans])
+x = r * np.cos(phi)
+y = r*np.sin(phi)
+ax.scatter(x,y, s=0.4, c=time[::-1], cmap='RdPu')
+ax.scatter(0,0, color='black')
+
+plt.axis(xmin = -350, xmax=350, ymin=-350, ymax=350)
+ax.set_aspect(1)
+plt.savefig(fname='swoosh1.png', transparent=True, dpi=1500)
+
+
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+M = 5.972e24 * u.kg
+pos_vec = np.array([200.0, np.pi/2, 0.5*(np.pi/2+(np.pi+np.pi/6))+4*np.pi/3])
+real_omega = 7600
+vel_vec = np.array([0, 0., real_omega])
+
+
+time = 0 * u.s
+cl = Schwarzschild.from_values(pos_vec, vel_vec, time, M)
+ANS = cl.calculate_trajectory(end_lambda=end_lambda/30, OdeMethodKwargs={'stepsize':0.2e-6})
+
+ans = ANS[1]
+print('calculate done')
+
+r = np.array([t[1] for t in ans])
+print(r[-1])
+
+phi = np.array([t[3] for t in ans])
+time = np.array([t[0] for t in ans])
+x = r * np.cos(phi)
+y = r*np.sin(phi)
+ax.scatter(x,y, s=0.4, c=time[::-1], cmap='RdPu')
+ax.scatter(0,0, color='black')
+
+plt.axis(xmin = -350, xmax=350, ymin=-350, ymax=350)
+ax.set_aspect(1)
+plt.savefig(fname='swoosh2.png', transparent=True, dpi=1500)
+
+
+
+
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
@@ -47,7 +151,7 @@ ax.scatter(0,0, color='black')
 plt.axis(xmin = -350, xmax=350, ymin=-350, ymax=350)
 ax.set_aspect(1)
 plt.savefig(fname='momo0.png', transparent=True, dpi=1500)
-
+# plt.show()
 
 
 
@@ -115,4 +219,5 @@ ax.scatter(0,0, color='black')
 
 plt.axis(xmin = -350, xmax=350, ymin=-350, ymax=350)
 ax.set_aspect(1)
-plt.savefig(fname='momo2.png', transparent=True, dpi=1500)
+# plt.savefig(fname='momo2.png', transparent=True, dpi=1500)
+plt.show()
